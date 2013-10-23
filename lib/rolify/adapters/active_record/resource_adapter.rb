0b674d13f425afbb3ae48f4d74d9fcdfef6fdc41
Josh Symonds <veraticus@gmail.com>
2013-10-23 14:19:00 -0500

    Fix query issues with ActiveRecord 4 adapters

--- lib/rolify/adapters/active_record/resource_adapter.rb
+++ lib/rolify/adapters/active_record/resource_adapter.rb
@@ -7,12 +7,12 @@ module Rolify
         resources = relation.joins("INNER JOIN #{quote(roles_table)} ON #{quote(roles_table)}.resource_type = '#{relation.to_s}' AND
                                     (#{quote(roles_table)}.resource_id IS NULL OR #{quote(roles_table)}.resource_id = #{quote(relation.table_name)}.#{relation.primary_key})")
         resources = resources.where("#{quote(roles_table)}.name IN (?) AND #{quote(roles_table)}.resource_type = ?", Array(role_name), relation.to_s)
-        resources = resources.select("#{quote(relation.table_name)}.*")
+        resources = resources.select("#{quote(relation.table_name)}.id")
         resources
       end
 
       def in(relation, user, role_names)
-        roles = user.roles.where(:name => role_names)
+        roles = user.roles.where(:name => role_names).pluck(:id)
         relation.where("#{quote(role_class.table_name)}.#{role_class.primary_key} IN (?) AND ((resource_id = #{quote(relation.table_name)}.#{relation.primary_key}) OR (resource_id IS NULL))", roles)
       end
 
